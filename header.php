906a5a72d9fdda957e084a70d6c8626224cee34f
Othagor <otiko.gochiashvili@gmail.com>
2025-01-26 16:04:02 +0400

    PHP code and deleted files

---
+++ header.php
@@ -0,0 +1,8 @@
+		<!-- Header -->
+        <div id="header">
+				<span class="logo icon fa-paper-plane"></span>
+				<h1>HI. this is Luka</h1>
+				<p>future web developer and 
+				<br />
+				graphic designer.</p>
+			</div>
---
+++ main.php
@@ -0,0 +1,47 @@
+<!-- Main -->
+<div id="main">
+
+<header class="major container medium">
+    <h2>This is the projects I worked on in
+    <br />
+    Skillwill and the projects which will be
+    <br />
+    the base of my future development</h2>
+    
+</header>
+
+<div class="box alt container">
+    <section class="feature left">
+        <a href="https://github.com/Othagor/Lux-Ventus-Skillwill" class="image icon solid fa-signal"><img src="./images/lux ventus.png" alt="" /></a>
+        <div class="content">
+            <h3>LUX VENTUS</h3>
+            <p>This is the site of the brand which will revolutionize the fashion world very soon.</p>
+        </div>
+    </section>
+    <section class="feature right">
+        <a href="https://github.com/Othagor/MICO" class="image icon solid fa-code"><img src="./images/mico.png" alt="" /></a>
+        <div class="content">
+            <h3>Mico</h3>
+            <p>most modern type of hospital, where every employee takes care of your health.</p>
+        </div>
+    </section>
+    <section class="feature left">
+        <a href="https://github.com/Othagor/Travel" class="image icon solid fa-mobile-alt"><img src="./images/travel.png" alt="" /></a>
+        <div class="content">
+            <h3>TRAVEL.</h3>
+            <p>Interested in adventuring around the world? contact TRAVEL. the best touristic company.</p>
+        </div>
+    </section>
+</div>
+
+
+
+<footer class="major container medium">
+    <h3>Wanna be my partner in future adventures?</h3>
+    <p>If you think you can build sites and have many hard but sweet adventures with me feel free to click on the button and send your CV.</p>
+    <ul class="actions special">
+        <li><a href="#" class="button">Join my crew</a></li>
+    </ul>
+</footer>
+
+</div>
